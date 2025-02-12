5e13bcae71c46ac00b64452be2bf6a5551095fc1
Paweł Kierski <pkierski@gmail.com>
2025-02-12 22:59:11 +0100

    Support for multiple listeners

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ POP3 server module in Go
  * [x] check if message is deleted and return error for DELE, RETR, LIST and other commands
  * [x] limit sessions count
  * [x] add timeout for idle/stale sessions
- * [ ] support for multiple `Server`/`ListenAndServe` calls
+ * [x] support for multiple `Server`/`ListenAndServe` calls
--- server.go
+++ server.go
@@ -29,11 +29,13 @@ type (
 		authorizer   Authorizer
 		mboxProvider MailboxProvider
 
-		listener     net.Listener
-		sessions     map[*Session]struct{}
-		sessionsMu   sync.Mutex
-		inShutdown   atomic.Bool
-		sessionsDone chan struct{}
+		inShutdown     atomic.Bool
+		listeners      map[*net.Listener]struct{}
+		listenersMu    sync.Mutex
+		listenersGroup sync.WaitGroup
+		sessions       map[*Session]struct{}
+		sessionsMu     sync.Mutex
+		sessionsDone   chan struct{}
 	}
 )
 
@@ -49,6 +51,7 @@ func NewServer(authorizer Authorizer, mboxProvider MailboxProvider) *Server {
 		ConnectionsLimit: DefaultConnectionsLimit,
 		authorizer:       authorizer,
 		mboxProvider:     mboxProvider,
+		listeners:        make(map[*net.Listener]struct{}),
 		sessions:         make(map[*Session]struct{}),
 		sessionsDone:     make(chan struct{}),
 	}
@@ -62,7 +65,15 @@ func NewServer(authorizer Authorizer, mboxProvider MailboxProvider) *Server {
 func (s *Server) Serve(l net.Listener) error {
 	// TODO: don't allow to add more listeners
 	// or add support for multiple listeners
-	s.listener = l
+
+	l = &onceCloseListener{Listener: l}
+	defer l.Close()
+
+	if !s.addListener(&l) {
+		return ErrServerClosed
+	}
+	defer s.removeListener(&l)
+
 	for {
 		conn, err := l.Accept()
 		if s.shuttingDown() {
@@ -71,7 +82,7 @@ func (s *Server) Serve(l net.Listener) error {
 		if err != nil {
 			return err
 		}
-		log.Printf("New connection from: %v", conn.RemoteAddr())
+		log.Printf("New connection from: %v on: %v", conn.RemoteAddr(), conn.LocalAddr())
 		session, err := NewSession(conn, s.mboxProvider, s.authorizer)
 		if err != nil {
 			conn.Close()
@@ -91,6 +102,7 @@ func (s *Server) Serve(l net.Listener) error {
 			if s.inShutdown.Load() && !s.hasActiveSessions() {
 				close(s.sessionsDone)
 			}
+			log.Printf("Connection from: %v on: %v closed", conn.RemoteAddr(), conn.LocalAddr())
 		}()
 	}
 }
@@ -116,8 +128,8 @@ func (s *Server) ListenAndServe(addr string) error {
 }
 
 // Shutdown gracefully shuts down the server without interrupting any
-// active connections. Shutdown works by first closing open
-// listener and then waiting indefinitely for connections to return
+// active connections. Shutdown works by first closing all open
+// listeners and then waiting indefinitely for connections to return
 // to idle and then shut down.
 // If the provided context expires before the shutdown is complete,
 // Shutdown returns the context's error, otherwise it returns any
@@ -130,12 +142,20 @@ func (s *Server) ListenAndServe(addr string) error {
 // Once Shutdown has been called on a server, it may not be reused;
 // future calls to methods such as Serve will return ErrServerClosed.
 func (s *Server) Shutdown(ctx context.Context) error {
-	s.inShutdown.Store(true)
-
-	var lnerr error
-	if s.listener == nil {
-		lnerr = s.listener.Close()
+	if !s.inShutdown.CompareAndSwap(false, true) {
+		return ErrServerClosed
 	}
+
+	s.listenersMu.Lock()
+	lnerr := s.closeListenersLocked()
+	s.listenersMu.Unlock()
+	s.listenersGroup.Wait()
+
+	s.sessionsMu.Lock()
+	if len(s.sessions) == 0 {
+		close(s.sessionsDone)
+	}
+	s.sessionsMu.Unlock()
 
 	select {
 	case <-ctx.Done():
@@ -147,22 +167,26 @@ func (s *Server) Shutdown(ctx context.Context) error {
 	return lnerr
 }
 
-// Close immediately closes active net.Listener and any
+// Close immediately closes all active net.Listener and any
 // connections. For a graceful shutdown, use [Server.Shutdown].
 //
 // Close returns any error returned from closing the [Server]'s
 // underlying Listener.
 func (s *Server) Close() error {
-	s.inShutdown.Store(true)
-
-	var lnerr error
-	if s.listener == nil {
-		lnerr = s.listener.Close()
+	if !s.inShutdown.CompareAndSwap(false, true) {
+		return ErrServerClosed
 	}
 
+	s.listenersMu.Lock()
+	lnerr := s.closeListenersLocked()
+	s.listenersMu.Unlock()
+	s.listenersGroup.Wait()
+
+	s.sessionsMu.Lock()
 	for session := range s.sessions {
 		session.conn.Close()
 	}
+	s.sessionsMu.Unlock()
 
 	return lnerr
 }
@@ -192,4 +216,47 @@ func (s *Server) hasActiveSessions() bool {
 	s.sessionsMu.Lock()
 	defer s.sessionsMu.Unlock()
 	return len(s.sessions) > 0
+}
+
+// onceCloseListener wraps a net.Listener, protecting it from
+// multiple Close calls.
+type onceCloseListener struct {
+	net.Listener
+	once     sync.Once
+	closeErr error
+}
+
+func (oc *onceCloseListener) Close() error {
+	oc.once.Do(oc.close)
+	return oc.closeErr
+}
+
+func (oc *onceCloseListener) close() { oc.closeErr = oc.Listener.Close() }
+
+func (s *Server) closeListenersLocked() error {
+	var err error
+	for ln := range s.listeners {
+		if cerr := (*ln).Close(); cerr != nil && err == nil {
+			err = cerr
+		}
+	}
+	return err
+}
+
+func (s *Server) addListener(ln *net.Listener) bool {
+	s.listenersMu.Lock()
+	defer s.listenersMu.Unlock()
+	if s.inShutdown.Load() {
+		return false
+	}
+	s.listeners[ln] = struct{}{}
+	s.listenersGroup.Add(1)
+	return true
+}
+
+func (s *Server) removeListener(ln *net.Listener) {
+	s.listenersMu.Lock()
+	defer s.listenersMu.Unlock()
+	delete(s.listeners, ln)
+	s.listenersGroup.Done()
 }
